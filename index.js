eff67b0d06ad0e865fe3b91a65abb0e9f38fcb0f
Saadh Zahid <86085628+Arkane29A@users.noreply.github.com>
2023-04-06 08:27:18 +0100

    maybe this

--- index.js
+++ index.js
@@ -9,7 +9,19 @@ const axios = require('axios');
 const cookieSession = require('cookie-session');
 const googleauth = require('./auth');
 const session = require('express-session');
+const { v4 } = require('uuid');
 
+app.get('/api', (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+app.get('/api/item/:slug', (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
+});
 app.get('/api', (req, res) => {
   const path = `/api/item/${v4()}`;
   res.setHeader('Content-Type', 'text/html');
