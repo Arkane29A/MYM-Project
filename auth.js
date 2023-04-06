aa045b111c79c774bb3b1e8ed0c5641efe8ede5f
Saadh Zahid <86085628+Arkane29A@users.noreply.github.com>
2023-04-06 20:16:49 +0100

    fixed callback

--- auth.js
+++ auth.js
@@ -24,7 +24,7 @@ passport.deserializeUser(function(user, done)
 passport.use(new GoogleStrategy({
   clientID: GOOGLE_CLIENT_ID,
   clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/google/callback"
+  callbackURL: 'production' ? "https://mymproject.herokuapp.com/google/callback" : "http://localhost:3000/google/callback"
 },
 function(accessToken, refreshToken, profile, cb) {
 
